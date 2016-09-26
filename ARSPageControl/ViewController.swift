878271c93d9d3ce0ccaf99de9c8dec463c11478b
Yarik Arsenkin <majesticy@gmail.com>
2016-09-26 23:24:02 +0200

    Initial Commit

---
+++ ARSPageControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARSPageControl
+//
+//  Created by Yaroslav Arsenkin on 26/09/2016.
+//  Copyright © 2016 Yarik Arsenkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
